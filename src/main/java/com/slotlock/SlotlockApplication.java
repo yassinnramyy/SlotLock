976da5ef306782ff73e8550277e92d2513470b99
Yassin Ramy <yassin.ramy2005@gmail.com>
2026-07-13 14:04:22 +0300

    Bootstrap and Dockerization

---
+++ src/main/java/com/slotlock/SlotlockApplication.java
@@ -0,0 +1,13 @@
+package com.slotlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlotlockApplication.class, args);
+	}
+
+}
